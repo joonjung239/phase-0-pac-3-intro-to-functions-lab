d8fb3ea4e2296b9a8bf9b8a8de0c4a00086a4bac
Joon Jung <joonjung55@gmail.com>
2022-05-03 22:03:48 -0400

    commit tree 5a1b32c879d37c402b0e78e0b03fb29cf51a356a

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  "hello".toLowerCase();
+  function whisper(string)
+  {return string.toLowerCase();}
+
+  function logShout(string) {
+      console.log(string.toUpperCase())
+  }
+
+  function logWhisper(string){
+      console.log(string.toLowerCase())
+  }
+  function sayHiToHeadphonedRoommate(string) 
+  {
+      if (string === string.toLowerCase()){
+          return "I can't hear you!"}
+     {if (string === string.toUpperCase())
+        return "YES INDEED!"}
+        {if (string === "Let's have dinner together!")
+        return "I would love to!"}
+  }
